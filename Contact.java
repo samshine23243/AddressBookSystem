9a866480006483b6794c0019bf5a2ced4a86ba9c
samshine <sameershinde23243@gmail.com>
2021-03-13 11:53:10 +0530

    [sameer] contact book code added

---
+++ Contact.java
@@ -0,0 +1,21 @@
+
+public class Contact {
+	String FIRST_NAME;
+	String LAST_NAME;
+	String ADDRESS;
+	String CITY;
+	String STATE;
+	String PHONE_NUMBER;
+	String EMAIL;
+	Contact(String fn,String ln,String add,String city,String state,String pn,String email)
+	{
+		this.FIRST_NAME=fn;
+		this.LAST_NAME=ln;
+		this.ADDRESS=add;
+		this.CITY=city;
+		this.STATE=state;
+		this.PHONE_NUMBER=pn;
+		this.EMAIL=email;
+	}
+
+}
