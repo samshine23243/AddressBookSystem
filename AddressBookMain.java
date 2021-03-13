d713ff43dd0199ac17161270afc5a4bd5f4297a1
samshine <sameershinde23243@gmail.com>
2021-03-13 12:57:37 +0530

    [sameer] contact delete code added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -22,6 +22,7 @@ public class AddressBookMain {
 		contacts[i++]=new Contact(FN, LN, ADD, City, State, PN, Email);
 		return i;	
 	}
+	
 	void edit(int i)
 	{
 		int temp;
@@ -49,23 +50,66 @@ public class AddressBookMain {
 		}
 		System.out.println("Editied Sucessfully...");
 	}
-
-
-
-
-
 	
-	
-	
-	
+	void delete(int i)
+	{
+		int temp;
+		for(temp=0;temp<10;temp++)
+		{
+			if(temp==i)
+			{
+				contacts[i]=null;
+				break;
+			}
+		}
+		System.out.println("deleted Sucessfully...");
+	}
+
+
+
+	void display()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i+" "+contacts[i]);
+		}
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
 		System.out.println("Welcome to Address Book Systemm Program !!!!");
 		AddressBookMain abm=new AddressBookMain();
-	
-		
+
+		String ans="Y";
+		int i=0;
+		do
+		{
+			System.out.println("1.Add 2.Edit 3.Delete");
+			int choice=s.nextInt();
+			switch(choice)
+			{
+				case 1:
+				i=abm.add(i);
+				abm.display();
+				break;
+				case 2:
+				System.out.println("Enter record number you want to edit:");
+				int j=s.nextInt();
+				abm.edit(j);
+				abm.display();
+				break;
+				case 3:
+				System.out.println("Enter record number you want to delete:");
+				int k=s.nextInt();
+				abm.delete(k);
+				abm.display();
+				break;
+			}
+			System.out.println("Enter choice(Y/N):");
+			ans=s.next();
+		}while(ans!="N");
 	}
 
 }
